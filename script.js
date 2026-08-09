133693bd26bf1b2697a97541445160e2d175ea39
jahphetB <japhetlakew098@gmail.com>
2026-08-08 22:42:32 -0600

    added the change grid size button

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-const container = document.querySelector("div");
+const container = document.querySelector(".container");
+const gridButton = document.querySelector(".grid-button");
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
+function createGrid(size) {
+    container.innerHTML = "";
+    const squareSize = 960/size;
 
-    square.style.width = "60px";
-    square.style.height = "60px";
+    for (let i = 0; i < size * size ; i++) {
+        const square = document.createElement("div");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
 
-    square.addEventListener("mouseenter", function () {
-        square.style.backgroundColor = "black";
-    });
+        square.addEventListener("mouseenter", function () {
+            square.style.backgroundColor = "black";
+        });
 
-    container.appendChild(square);
-    
+        container.appendChild(square);
+    }
 }
+
+createGrid(16);
+
+gridButton.addEventListener("click", function () {
+    let size = prompt("Enter the number of squares per side (maximum 100):");
+    size = Number(size);
+
+    if (size > 0 && size <= 100) {
+        createGrid(size);
+    } else {
+        alert("please enter a number between 1 and 100.");
+    }
+})
